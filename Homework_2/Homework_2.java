17bbad0e1b20f3213e3d1b17976c5751f8968a2f
Stanislav Tkachenko <infastiko@gmail.com>
2023-03-21 20:11:53 +0400

    added saveFile method

--- Homework_2/Homework_2.java
+++ Homework_2/Homework_2.java
@@ -13,3 +13,25 @@ static String buildString(int n, char c1, char c2) {
         return sb.toString();
     } else return "n not even. Exit program.";
 }
+
+static void saveFile(String createdString) {
+    try {
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("/file_ab.txt");
+        File file = new File(pathFile);
+
+        if (file.createNewFile()) {
+            System.out.println("file.created");
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(createdString);
+            fileWriter.flush();
+            fileWriter.close();
+        } else {
+            System.out.println("file.existed");
+        }
+    } catch (Exception e) {
+        e.getMessage();
+    } finally {
+        System.out.println(createdString);
+    }
+}
