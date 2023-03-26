201997381be2183f02ebcfd5012e4a1161afb1f1
Stanislav Tkachenko <infastiko@gmail.com>
2023-03-26 19:06:46 +0400

    added main

--- Homework_4/Homework_4.java
+++ Homework_4/Homework_4.java
@@ -9,7 +9,7 @@ public class Homework_4 {
         String s = null;
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print( "Введите строку вида, например string~1 : " );
+            System.out.print( "Введите строку (например string~1): " );
             s = br.readLine().strip();
         } catch (IOException e) { System.out.println(e.getMessage()); }
         return s;
@@ -26,5 +26,36 @@ public class Homework_4 {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    public static void main(String[] args) {
+
+        String s = input();
+        while(!validation( s )) {
+            System.out.println("Неправильный формат. Повторите попытку: ");
+            s = input();
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        while(!s.equals( "exit" )) {
+            String[] str = s.split( "~" );
+            int i = Integer.parseInt( str[1] );
+            int defI = i - list.size();
+            if (defI >= 0) {
+                while (defI != 0) {
+                    list.add( null );
+                    defI--;
+                }
+            }
+            if (str[0].equals( "print" )) {
+                System.out.println("\n" + list.get( Integer.parseInt( str[1] ) - 1  ));
+            } else { list.set(i-1, str[0]); }
+
+            System.out.println("\nЕсли хотите выйти из программы, введите exit");
+            System.out.println("Для вывода нужного элемента массива, ввeдите строку фомата (print~1): ");
+            s = input();
+        }
+
     }
 }
