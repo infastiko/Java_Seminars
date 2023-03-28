286d308858b5c73af35f124d7282770a8d00c538
Stanislav Tkachenko <infastiko@gmail.com>
2023-03-28 21:25:41 +0400

    added collectStats method

---
+++ Homework_5/Homework_5.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Homework_5 {
+    static void collectStats(String text) {
+        Map<Integer, List<String>> stats = new HashMap<>();
+        String[] list = text.split( " " );
+        for (String item: list) {
+            if (!stats.containsKey( item.length() )) {
+                stats.put( item.length(), new ArrayList<>( List.of( item ) ) );
+            }
+            else { stats.get(item.length()).add( item ); }
+        }
+
+        for (var item : stats.entrySet()) {
+            List<String> printW = item.getValue();
+            for (String s : printW) { System.out.print( s + " " ); }
+            System.out.println();
+        }
+    }
+    
+}
